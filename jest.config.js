f3838637408dfaeb15f9ef886608a36efc288926
Danilo Gonçalves <gonca.danilo@gmail.com>
2020-05-18 18:03:41 -0300

    chore: update jest settings

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,18 @@
+const { pathsToModuleNameMapper } = require('ts-jest/utils');
+const { compilerOptions } = require('./tsconfig.json');
+
 module.exports = {
   clearMocks: true,
-  collectCoverageFrom: ['src/**/*.ts', '!src/server.ts', '!src/schemas/*'],
+  collectCoverageFrom: [
+    '<rootDir>/src/modules/**/services/*.ts',
+    '<rootDir>/src/modules/**/providers/**/*.ts'
+  ],
   coverageDirectory: 'coverage',
+  coverageReporters: ['text-summary', 'lcov'],
+  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, {
+    prefix: '<rootDir>/src/',
+  }),
+  preset: 'ts-jest',
   testEnvironment: 'node',
   testMatch: ['**/*.spec.ts'],
   transform: {
