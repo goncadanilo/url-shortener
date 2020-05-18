f5085cfe922020803207da8cda477f64010db471
Danilo Gonçalves <gonca.danilo@gmail.com>
2020-05-18 19:35:04 -0300

    refactor: add dependency injection control

--- src/modules/urls/services/ShortenUrlService.ts
+++ src/modules/urls/services/ShortenUrlService.ts
@@ -1,20 +1,26 @@
+import { injectable, inject } from 'tsyringe';
+
 import HttpError from '@shared/errors/HttpError';
 import IUrlRepository from '@modules/urls/repositories/IUrlRepository';
-import IUrlValidationProvider from '@modules/urls/providers/UrlValidation/models/IUrlValidation';
+import IUrlValidation from '@modules/urls/providers/UrlValidation/models/IUrlValidation';
 
 interface IResponse {
   status: number;
   newUrl: string;
 }
 
+@injectable()
 class ShortenUrlService {
   constructor(
+    @inject('UrlRepository')
     private readonly urlRepository: IUrlRepository,
-    private readonly urlValidationProvider: IUrlValidationProvider,
+
+    @inject('UrlValidation')
+    private readonly urlValidation: IUrlValidation,
   ) {}
 
   public async execute(originalUrl: string): Promise<IResponse> {
-    if (!this.urlValidationProvider.test(originalUrl))
+    if (!this.urlValidation.test(originalUrl))
       throw new HttpError('Invalid url');
 
     const newUrl = await this.urlRepository.create(originalUrl);
