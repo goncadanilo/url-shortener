3541735208b4fe69d670a136b4944d5973707bce
Danilo Gonçalves <gonca.danilo@gmail.com>
2020-05-18 19:30:42 -0300

    refactor: renamed files

--- src/modules/urls/providers/UrlValidation/implementations/UrlValidation.spec.ts
+++ src/modules/urls/providers/UrlValidation/implementations/UrlValidation.spec.ts
@@ -1,20 +1,20 @@
 import faker from 'faker';
 
-import UrlValidationProvider from './UrlValidationProvider';
+import UrlValidation from './UrlValidation';
 
 describe('Url Validation Provider', () => {
   it('should returns false if an invalid url is provided', () => {
-    const urlValidationProvider = new UrlValidationProvider();
+    const urlValidation = new UrlValidation();
     const url = 'any_url';
-    const response = urlValidationProvider.test(url);
+    const response = urlValidation.test(url);
 
     expect(response).toBe(false);
   });
 
   it('should returns true if an valid url is provided', () => {
-    const urlValidationProvider = new UrlValidationProvider();
+    const urlValidation = new UrlValidation();
     const url = faker.internet.url();
-    const response = urlValidationProvider.test(url);
+    const response = urlValidation.test(url);
 
     expect(response).toBe(true);
   });
